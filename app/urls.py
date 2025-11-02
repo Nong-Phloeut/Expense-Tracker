bcc399ef678b263e6a2a6442d37a9245ddb01fcc
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2025-11-02 14:36:28 +0700

    update logout

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views  # ← This imports auth_views
 
 urlpatterns = [
     path('', views.login_view, name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('users', views.user_management, name='user_management'),
     path('expenses', views.expense_entry, name='expense_entry'),
