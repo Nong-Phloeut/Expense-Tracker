91b15d37e4240c79fd2f8ece765edffc10d134a4
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2025-11-04 23:14:56 +0700

    update column

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.BooleanField(default=False)
+    description = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
