bbc7ca406edde1fbc8e22df50d804c26a0977002
ONG-PHLOEUT-GITHUB <phloeutnong@gmail.com>
2025-11-04 21:31:10 +0700

    update alert

--- app/views/category_views.py
+++ app/views/category_views.py
@@ -6,21 +6,21 @@ from django.core.paginator import Paginator
 
 @login_required(login_url='login')
 def category_management(request):
-    category_id = request.GET.get('id')
-    category_to_edit = None
-    if category_id:
-        category_to_edit = get_object_or_404(Category, id=category_id)
-
     if request.method == 'POST':
+        category_id = request.POST.get('category_id')
         name = request.POST.get('name')
-        if category_to_edit:
-            category_to_edit.name = name
-            category_to_edit.save()
+
+        if category_id:
+            # Update existing category
+            category = get_object_or_404(Category, id=category_id)
+            category.name = name
+            category.save()
             messages.success(request, f"Category '{name}' updated successfully!")
         else:
+            # Create new category
             Category.objects.create(name=name)
             messages.success(request, f"Category '{name}' added successfully!")
-        return redirect('category_management')
+
 
     all_categories = Category.objects.all().order_by('id')
     paginator = Paginator(all_categories, 10)
@@ -28,8 +28,7 @@ def category_management(request):
     categories = paginator.get_page(page_number)
 
     return render(request, 'category_management/categories.html', {
-        'categories': categories,
-        'edit_category': category_to_edit
+        'categories': categories
     })
 
 @login_required(login_url='login')
